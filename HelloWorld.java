bddd6a018853a4e5ca3a87f72c73fe5a8ef2d42e
vinaybathini444 <142766437+vinaybathini444@users.noreply.github.com>
2023-08-26 02:15:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class Pratical{
    public static void main(String[] args) {
-        System.out.println("Hello world! this is vinay kumar doing practial-13.");
+        System.out.println("Hello world! this is vinay kumar doing practial-14");
     }
 }
